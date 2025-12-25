423108e7b0cf949eb13415fc23a10e1e8a22ace3
NwayOoPyaeZo <nwayoopyaezo@gmail.com>
2025-12-25 11:22:41 +0700

    add PostgreSQL connection setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const app = require("./app");
 require("dotenv").config();
+const pool = require("./db/db");
 
 const PORT = process.env.PORT || 5000;
+
+pool
+  .connect()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch((err) => console.error("DB connection error", err));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
