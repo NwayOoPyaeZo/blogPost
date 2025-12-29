0f229e9d42f8f56df572d61005c2b4267eb85159
NwayOoPyaeZo <nwayoopyaezo@gmail.com>
2025-12-29 09:59:13 +0700

    prepare for render deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -8,10 +9,18 @@ const postsRoutes = require("./routes/posts.routes");
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use("/posts", postsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
+});
+
+// Catch-all route to serve frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = app;
